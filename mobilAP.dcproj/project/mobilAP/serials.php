94ad6107ffe8636f72d233bd166c99875488180c
Pete Akins <pete@ideadrop.com>
2010-03-08 02:11:20 +0000

    serials return zero when content is private and user is not logged in

--- mobilAP.dcproj/project/mobilAP/serials.php
+++ mobilAP.dcproj/project/mobilAP/serials.php
@@ -1,11 +1,20 @@
 <?php
 
 require_once('../mobilAP.php');
+$user_session = new mobilAP_UserSession();
 
 if (isset($_GET['key'])) {
     $data = array($_GET['key']=>mobilAP::getSerialValue($_GET['key']));
 } else {
     $data = mobilAP::getSerials();
+}
+
+if (mobilAP::getConfig('CONTENT_PRIVATE') && !$user_session->loggedIn()) {
+	foreach ($data as $key=>$value) {
+		if ($key != 'config') {
+			$data[$key] = 0;
+		}
+	}
 }
 
 header("Content-type: application/json; charset=" . MOBILAP_CHARSET);
