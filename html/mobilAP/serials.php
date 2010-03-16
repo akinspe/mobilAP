19058f03955166900e36924353d1fe67a0244abf
Pete Akins <pete@ideadrop.com>
2010-03-16 18:59:30 +0000

    updated serials to only return data values and the serial for the current user added resetSerials method to clean out stale data

--- html/mobilAP/serials.php
+++ html/mobilAP/serials.php
@@ -3,10 +3,18 @@
 require_once('../mobilAP.php');
 $user_session = new mobilAP_UserSession();
 
+if (isset($_GET['reset'])) {
+	$user = $user_session->getUser();
+	if ($user->isSiteAdmin()) {
+		mobilAP::resetSerials();
+	}
+}
+
 if (isset($_GET['key'])) {
     $data = array($_GET['key']=>mobilAP::getSerialValue($_GET['key']));
 } else {
-    $data = mobilAP::getSerials();
+    $data = mobilAP::getSerials(mobilAP::SERIAL_TYPE_DATA);
+	$data['user'] = $user_session->loggedIn() ? mobilAP::getSerialValue($user_session->getUserID()) : 0;
 }
 
 if (mobilAP::getConfig('CONTENT_PRIVATE') && !$user_session->loggedIn()) {
